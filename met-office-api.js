44c3c5e5d970318684b8909271cf05cc8d9cc2ca
chris-mark-wilson <cmwilson.co.uk>
2024-01-28 19:06:33 +0000

    animating radar imagery

--- met-office-api.js
+++ met-office-api.js
@@ -2,12 +2,16 @@ import axios from 'axios';
 import {MET_API} from '@env';   
 
 export const getCapabilities = async () => {
+
+    //wxfcs = forecast data
+    //wxobs = observation data
     const MET_URL="http://datapoint.metoffice.gov.uk/public/data/";
     const resource="layer/wxfcs/all/json/capabilities?key=";
     const key=MET_API;
     const url=`${MET_URL}${resource}${key}`;
     try{
     const response = await axios.get(url);
+    console.log(JSON.stringify(response.data,null,1),"response data in weather map api")
     return response.data;
     }
     catch (error){
--- scenes/components/weatherMap.js
+++ scenes/components/weatherMap.js
@@ -1,8 +1,12 @@
-import { View, Text, StyleSheet} from "react-native"
+import { View, Text, StyleSheet,Image,Pressable} from "react-native"
 import { useWindowDimensions } from "react-native"
 import { getMapOverlayUrls } from "../../functions/getMapOverlayUrls"
 import { downloadMapOverlays } from "../../functions/downloadMapOverlays"
-import {useEffect,useState} from "react"
+import {useEffect,useState,useContext} from "react"
+import { IconContext } from "../../contexts/iconContext";
+
+import {API_KEY} from "@env"
+
 
 
 
@@ -13,8 +17,19 @@ import {useEffect,useState} from "react"
 
 
  export const WeatherMap=({location})=>{
+  const staticMapUrl=`https://maps.googleapis.com/maps/api/staticmap?center=54.5,-3.5&markers=color:red|size:tiny|${location.lat},${location.lon}&scale=2&zoom=5&size=515x515&maptype=terrain&key=${API_KEY}`
+
+  const { SVG } = useContext(IconContext);
 
   const [isUK,setIsUK]=useState(false);
+
+  const [uriList,setUriList]=useState({});
+  const [isLoading,setIsLoading]=useState(true);
+  const [defaultTimes,setDefaultTimes]=useState({});//default times for each layer
+  const [offsetHours,setOffsetHours]=useState(0);//offset hours for each layer
+  const [count,setCount]=useState(0);//counter for timing sequence of images
+  const interval=2000;
+  const[displayTime,setDisplayTime]=useState(0);//time to display on screen
  
     useEffect(() => {
       if (location.lat && location.lon) { 
@@ -27,9 +42,10 @@ import {useEffect,useState} from "react"
         } 
       }
     }, [location]);
-
+  
     useEffect(() => {
       if (isUK) {
+        setIsLoading(true);
         getMapOverlayUrls()
         .then((urlList) => {
             //urlList structure is {layerName:{timesteps:{timestep(number):url},defaultTime:time in iso format}} timestep is the FORECAST time/element of the url
@@ -45,37 +61,123 @@ import {useEffect,useState} from "react"
             // defaultTime is the start time of the sequence in iso format
          
             // console.log(JSON.stringify(urlList,null,1),"url list in weather map")
+            for(const layerName in urlList){
+              // get default time for each layer
+              defaultTimes[layerName]=urlList[layerName].defaultTime;
+            }
+
+            // console.log(JSON.stringify(defaultTimes,null,1),"default times in weather map")
+            const hours=3*60*60*1000;//3 hours in milliseconds
+            console.log(new Date(defaultTimes.Rainfall).toLocaleTimeString('en-US',{hour12:true}),"default time in weather map")
+
             downloadMapOverlays(urlList)
             .then((uriList)=>{ 
-              console.log(JSON.stringify(uriList,null,1),"uri list in weather map")
+              // uriList structure is {layerName:{timestep(number):uri}} timestep is the FORECAST time/element of the url
+              // e.g. {CloudAndRain:{0:"file://.../CloudAndRain_0.png",3:"file://.../CloudAndRain_3.png",6:"file://.../CloudAndRain_6.png"}}
+              
+              // console.log(JSON.stringify(uriList,null,1),"uri list in weather map")
+              setUriList(uriList);
+              setIsLoading(false);
 
 
              })
 
           })
-          .catch((err) => console.log(err));
+          .catch((err) => console.log(err, "error in weather map"));
       }
     }, [isUK]);
+
+    useEffect(()=>{
+      if(!isLoading){
+        setTimeout(()=>{
+          setOffsetHours((prev)=>{
+            if(prev===36) return 0
+            else return prev+3
+          });
+          setCount((count)=>count+1);
+        },interval)
+      }
+
+    },[count,isLoading])
+
+    useEffect(()=>{
+      setDisplayTime(()=>{
+        const time=new Date(defaultTimes.Rainfall);
+        time.setHours(time.getHours()+offsetHours);
+        return time.toString().slice(0,21);
+      })
+      if(offsetHours===36){
+        setOffsetHours(0);
+      }
+    },[offsetHours])
 
 
 
     const layout = useWindowDimensions();
     // console.log(layout.height,"layout height in weather map")
 
-    return (
-        <View style={{...styles.container,height:+`${layout.height}`}}>
-            <Text>Weather Map</Text>
+    return isLoading?<Text>loading</Text>:
+    (
+        <View style={{flex:1,height:500}}>
 
-            {isUK?<Text>{location.lat},{location.lon}{isUK}</Text>:<Text>Not in uk no radar data available</Text>}
+{/* style={{...styles.container,height:+`${layout.height}`}} */}
 
+        {!isUK&&<Text>Not in uk no radar data available</Text>}
+           <Image style={{...styles.images}} source={{uri:uriList.Rainfall[offsetHours]}}/> 
+           <Image style={{...styles.map,width:layout.width}} source={{uri:staticMapUrl}}/>
+           <Text style={styles.time}> {displayTime} </Text>
+
+      <Pressable  onPress={()=>console.log("press")}>
+        <View style={{...styles.cloudIcon}}>
+        <SVG.cloud_icon />
         </View>
+        </Pressable>
+         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-       height:"200",
-        width:"100%",
-        backgroundColor:"lightblue"
+      flex:1,
+        backgroundColor:"lightblue",
+      
+      
+    },
+    map:{
+      position:"absolute",
+      zIndex:-1,
+      height:550,
+  
+      
+    },
+    images:{
+      position:"absolute",
+      opacity:0.5,
+      zIndex:2,
+       width:"100%",
+       height:500,
+        borderWidth:1,
+        borderColor:"red",
+        paddingLeft:"15",
+        paddingRight:"15",
+        // marginLeft:"15%",
+        // marginRight:"15%",
+    },
+    cloudIcon:{
+      zIndex:999,
+      position:"absolute",
+      top:10,
+      right:20,
+      width:30,
+      height:30,
+    
+    },
+    time:{
+      zIndex:3,
+       position:"absolute",
+       backgroundColor:"white",
+       fontSize:16,
+       bottom:20,
+        left:20, 
     }
 })
